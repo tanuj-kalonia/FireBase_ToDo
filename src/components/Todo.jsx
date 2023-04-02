7cf5cabcf5ef1b4f27472fd7d67737d3bf0a55d6
Tanuj Kalonia <tanujkalonia2002@gmail.com>
2023-04-02 12:46:52 +0530

    Render Todo items

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
+import { ArrowClockwise, CheckCircleFill, Circle, Trash } from 'react-bootstrap-icons'
 
-const Todo = () => {
+const Todo = ({ todo }) => {
+
+    const [hover, setHover] = React.useState(false);
+
     return (
-        <div>Todo</div>
+        <div className="Todo">
+            <div
+                className="todo-container"
+                onMouseEnter={() => setHover(true)}
+                onMouseLeave={() => setHover(false)}
+            >
+                <div className="check-todo">
+                    {
+                        todo.checked ?
+                            <span className='checked'>
+                                <CheckCircleFill color='#bebebe' />
+                            </span>
+                            :
+                            <span className='unchecked'>
+                                <Circle color={todo.color} />
+                            </span>
+
+                    }
+                </div>
+
+                <div className="text">
+                    <p
+                        style={{ color: todo.checked ? '#bebebe' : '#000' }}
+                    >
+                        {todo.name}
+                    </p>
+                    <span>{todo.time} - {todo.project}</span>
+                    <div className={`line ${todo.checked ? ' line-through' : ''}`}></div>
+                </div>
+
+                <div className="add-to-next-day">
+                    {
+                        todo.checked &&
+                        <span>
+                            <ArrowClockwise />
+                        </span>
+                    }
+                </div>
+
+                <div className="delete-todo">
+                    {
+                        (hover || todo.checked) &&
+                        <span>
+                            <Trash />
+                        </span>
+                    }
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
+import Todo from './Todo'
+import Next7Days from './Next7Days';
 
 const Todos = () => {
+    const selectedProject = 'today';
+
+    const todos = [
+        {
+            id: 'asfsf34fas',
+            name: 'DO this',
+            time: '09',
+            data: "12312",
+            day: '1',
+            checked: true,
+            color: '$34rft5',
+            project: 'work'
+
+        },
+        {
+            id: 'asfsffas',
+            name: 'HEllo word this',
+            time: '09',
+            data: "12312",
+            day: '6',
+            checked: false,
+            color: '$34rfd5',
+            project: 'work'
+
+        }
+    ]
     return (
-        <div>Todos</div>
+        <div className="Todos">
+            <div className="selected-project">
+                {selectedProject}
+            </div>
+
+            <div className="todos">
+                {
+                    selectedProject === 'Next 7 days' ?
+                        <Next7Days todos={todos} />
+                        :
+                        todos.map(todo => (
+                            <Todo todo={todo} key={todo.id} />
+                        ))
+                }
+            </div>
+        </div>
     )
 }
 
-export default Todos
+export default Todos;
